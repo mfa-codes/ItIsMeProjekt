920fefb56e465fc0a3a0875c5c45a998fc1cfa4d
MFA <140258080+mfa-codes@users.noreply.github.com>
2023-08-06 18:19:10 +0200

    Update main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,25 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-  bild = ""
-  name = ""
-  str = ""
-  plz = ""
-  geburtsdatum = ""
-  sternzeichen = ""
-  familienstand = ""
+  bild = ""  // Foto
+  name = ""  // name
+  str = ""  //street
+  plz = ""  //Postal code 
+  geburtsdatum = ""  // birthday
+  sternzeichen = ""  //Zodiac sign 
+  familienstand = ""  // family status (single)
   tel = ""
-  vorname = ""
-  hausnummer = ""
-  ort= ""
-  geburtsort= ""
-  nationatilat= ""
-  auto= ""
+  vorname = "" //first name
+  hausnummer = "" //haus number
+  ort= ""   //place
+  geburtsort= "" //birthplace
+  nationatilat= "" //NATIONATILATE
+  auto= "" //Driving license 
   email= ""
-  beruf = "" + "" + ""
-  sprachen = "" + "" + ""
-  kenntnisse = "" + "" + ""
-  hobbies = "" + "" + ""
+  beruf = "" + "" + "" //profession You can enter more than one input but please separate them with ",".
+  sprachen = "" + "" + "" //languages You can enter more than one input but please separate them with ",".
+  kenntnisse = "" + "" + "" //knowledge You can enter more than one input but please separate them with ",".
+  hobbies = "" + "" + "" //You can enter more than one input but please separate them with ",".
 
   sprachenArray: string[] = [];
   hobbiesArray: string[] = [];
